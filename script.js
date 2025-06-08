f42df9db866ff8dc1a760e17a2f03c3e7b292bfb
TopboySaint <topboysaint1@gmail.com>
2025-06-08 01:06:28 +0100

    Working through sign up page for unregistered students

---
+++ script.js
@@ -0,0 +1,15 @@
+const errorMsg = document.getElementById("errorMsg")
+const usernameInput = document.getElementById("floating").value.trim()
+const passwordInput = document.getElementById("floatingP").value.trim()
+const rememberInput = document.getElementById("rem-me").value
+
+loginUser.addEventListener('click', (e)=>{
+    //  alert('yes i am working')
+    if(usernameInput === "" || passwordInput === ""){
+        errorMsg.style.display = "block"
+    }else{
+        errorMsg.style.display = "none"
+        // e.submit()
+        window.location.href = "user.html"
+    }
+})
