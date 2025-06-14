ea6e4ba75549f9d143430fd14798f11d237cc689
TopboySaint <topboysaint1@gmail.com>
2025-06-14 06:39:15 +0100

    Refactor HTML and CSS for improved readability;

--- signup.js
+++ signup.js
@@ -1,18 +1,18 @@
-const errorMsg = document.getElementById("errorMsg")
-const nameInput = document.getElementById("floatingName").value.trim()
-const mailInput = document.getElementById("floatingMail").value.trim()
-const phoneInput = document.getElementById("floatingNumber").value.trim()
-const usernameInput = document.getElementById("floatingUser").value.trim()
-const passwordInput = document.getElementById("floatingPass").value.trim()
-const rememberInput = document.getElementById("rem-me").value
+const errorMsg = document.getElementById("errorMsg");
+const nameInput = document.getElementById("floatingName").value.trim();
+const mailInput = document.getElementById("floatingMail").value.trim();
+const phoneInput = document.getElementById("floatingNumber").value.trim();
+const usernameInput = document.getElementById("floatingUser").value.trim();
+const passwordInput = document.getElementById("floatingPass").value.trim();
+const rememberInput = document.getElementById("rem-me").value;
 
-signUpUser.addEventListener('click', (e)=>{
-    //  alert('yes i am working')
-    if(usernameInput === "" || passwordInput === ""){
-        errorMsg.style.display = "block"
-    }else{
-        errorMsg.style.display = "none"
-        // e.submit()
-        // window.location.href = "user.html"
-    }
-})
+signUpUser.addEventListener("click", (e) => {
+  //  alert('yes i am working')
+  if (usernameInput === "" || passwordInput === "") {
+    errorMsg.style.display = "block";
+  } else {
+    errorMsg.style.display = "none";
+    // e.submit()
+    // window.location.href = "user.html"
+  }
+});
